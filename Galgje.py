21345682e7aefee404fa85bfa7120bbaa17f5543
Wolfox <iris.postma@coderclass.nl>
2017-10-02 10:48:58 +0200

    added word guessing

--- Galgje.py
+++ Galgje.py
@@ -2,12 +2,15 @@ while True:
     print("Welkom bij galgje, probeer het woord te raden, als je wil stoppen typ QQ, als je het woord wil raden typ ?.")
     W = "CSharp"
     G = 0
-    L = input("Geef EEN letter")
+    L = input("Geef een letter")
     if (L == "?"):
         GW = input("Wat denk je dat het woord is?")
         if (GW == "CSharp" or "csharp" or "Csharp" or "cSharp" or "CSHARP") :
             print("Goed geraden!")
-        else
+            break
+        else:
+            print("Sorry, dat was fout.")
     elif not L.isalpha():
+        print("ERROR: Je mag alleen letters invoeren")
 
 
