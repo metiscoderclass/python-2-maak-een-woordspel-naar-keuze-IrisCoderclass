72dfa8b3a727f084a5e8ad611ad2518bc0ffe019
Wolfox <iris.postma@coderclass.nl>
2017-10-02 10:41:57 +0200

    added more python code

--- Galgje.py
+++ Galgje.py
@@ -1,1 +1,13 @@
-import turtle
+while True:
+    print("Welkom bij galgje, probeer het woord te raden, als je wil stoppen typ QQ, als je het woord wil raden typ ?.")
+    W = "CSharp"
+    G = 0
+    L = input("Geef EEN letter")
+    if (L == "?"):
+        GW = input("Wat denk je dat het woord is?")
+        if (GW == "CSharp" or "csharp" or "Csharp" or "cSharp" or "CSHARP") :
+            print("Goed geraden!")
+        else
+    elif not L.isalpha():
+
+
