13b79c75dd58e6c02222e29af4f3b1690c43e130
Wolfox <iris.postma@coderclass.nl>
2017-10-05 10:22:37 +0200

    -

--- Galgje.py
+++ Galgje.py
@@ -7,6 +7,25 @@ while True:
     GL = "Je goed geraden letters zijn: "
     FL = "Je fout geraden letters zijn: "
 
+    if (L == "?"):
+        GW = input("Wat denk je dat het woord is?")
+        GW = GW.lower()
+        if (GW == W) :
+            print("Goed geraden!")
+            break
+        else:
+            print("Sorry, dat was fout.")
+            G = G + 1
+            print(GL)
+            print(FL)
+    elif not L.isalpha():
+        print("ERROR: Je mag alleen letters invoeren")
+    if (L == "QQ"):
+        print("Doei")
+        break
+    if (G == 5):
+        print("Sorry je hebt verloren, het woord was CSharp")
+        break
     if (L == "c" or "s" or "h" or "a" or "r" or "p"):
         GL = GL + L
         AGL = len(GL)
@@ -27,25 +46,3 @@ while True:
         G = G + 1
         print(GL)
         print(FL)
-    if (L == "?"):
-        GW = input("Wat denk je dat het woord is?")
-        GW = GW.lower()
-        if (GW == W) :
-            print("Goed geraden!")
-            break
-        else:
-            print("Sorry, dat was fout.")
-            G = G + 1
-            print(GL)
-            print(FL)
-    elif not L.isalpha():
-        print("ERROR: Je mag alleen letters invoeren")
-    if (L == "QQ"):
-        print("Doei")
-        break
-    if (G == 5):
-        print("Sorry je hebt verloren, het woord was CSharp")
-        break
-
-
-
