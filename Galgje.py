8579f0de816f79f290e5ba01a12eb1d9c07c6910
Wolfox <iris.postma@coderclass.nl>
2017-10-05 10:13:49 +0200

    goede en foute letters toegevoegd

--- Galgje.py
+++ Galgje.py
@@ -4,10 +4,14 @@ while True:
     G = 0
     L = input("Geef een letter")
     L = L.lower()
+    GL = "Je goed geraden letters zijn: "
+    FL = "Je fout geraden letters zijn: "
 
     if (L == "c" or "s" or "h" or "a" or "r" or "p"):
         GL = GL + L
         AGL = len(GL)
+        print (GL)
+        print (FL)
         if (AGL == 6):
             GWL = input("Wat denk je dat het woord is?")
             GWL = GWL.lower()
@@ -17,9 +21,12 @@ while True:
             else:
                 print("Sorry, dat was fout.")
                 G = G + 1
+                print(GL)
     if not (L == "c" or "s" or "h" or "a" or "r" or "p"):
         print("Dat was fout")
         G = G + 1
+        print(GL)
+        print(FL)
     if (L == "?"):
         GW = input("Wat denk je dat het woord is?")
         GW = GW.lower()
@@ -29,6 +36,8 @@ while True:
         else:
             print("Sorry, dat was fout.")
             G = G + 1
+            print(GL)
+            print(FL)
     elif not L.isalpha():
         print("ERROR: Je mag alleen letters invoeren")
     if (L == "QQ"):
