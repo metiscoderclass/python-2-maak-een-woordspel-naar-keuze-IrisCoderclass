995223b6de5d95e876023bfb84da5aa05f68df3f
Wolfox <iris.postma@coderclass.nl>
2017-10-06 09:17:41 +0200

    fixed mistakes program is now functional, changed word

--- Galgje.py
+++ Galgje.py
@@ -1,11 +1,13 @@
+GL = ""
+FL = ""
+W = "metis"
+G = 0
+AL = len(W)
+
 while True:
     print("Welkom bij galgje, probeer het woord te raden, als je wil stoppen typ QQ, als je het woord wil raden typ ?.")
-    W = "csharp"
-    G = 0
     L = input("Geef een letter")
     L = L.lower()
-    GL = "Je goed geraden letters zijn: "
-    FL = "Je fout geraden letters zijn: "
 
     if (L == "?"):
         GW = input("Wat denk je dat het woord is?")
@@ -16,22 +18,23 @@ while True:
         else:
             print("Sorry, dat was fout.")
             G = G + 1
-            print(GL)
-            print(FL)
+            print("Je goed geraden letters zijn: " + GL)
+            print("Je fout geraden letters zijn: " + FL)
     elif not L.isalpha():
         print("ERROR: Je mag alleen letters invoeren")
     if (L == "QQ"):
         print("Doei")
         break
-    if (G == 5):
-        print("Sorry je hebt verloren, het woord was CSharp")
-        break
-    if (L == "c" or "s" or "h" or "a" or "r" or "p"):
+    if L in GL:
+        print("Deze letter heb je al een keer geraden.")
+    elif L in FL:
+        print("Deze letter heb je al een keer geraden.")
+    elif L in W:
         GL = GL + L
         AGL = len(GL)
-        print (GL)
-        print (FL)
-        if (AGL == 6):
+        print("Je goed geraden letters zijn: " + GL)
+        print("Je fout geraden letters zijn: " + FL)
+        if (AGL == AL):
             GWL = input("Wat denk je dat het woord is?")
             GWL = GWL.lower()
             if (GWL == W):
@@ -40,9 +43,13 @@ while True:
             else:
                 print("Sorry, dat was fout.")
                 G = G + 1
-                print(GL)
-    if not (L == "c" or "s" or "h" or "a" or "r" or "p"):
+                print("Je goed geraden letters zijn: " + GL)
+    else:
         print("Dat was fout")
         G = G + 1
-        print(GL)
-        print(FL)
+        FL = FL + L
+        print("Je goed geraden letters zijn: " + GL)
+        print("Je fout geraden letters zijn: " + FL)
+    if (G == 5):
+        print("Sorry je hebt verloren, het woord was " + W)
+        break
