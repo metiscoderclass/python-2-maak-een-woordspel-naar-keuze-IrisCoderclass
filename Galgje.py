ef0dfdeb7e124f8df639acb9540da90da6c4cfda
Wolfox <iris.postma@coderclass.nl>
2017-10-09 10:20:16 +0200

    made it so it doesn't work if there's more than one letter

--- Galgje.py
+++ Galgje.py
@@ -2,14 +2,22 @@ GL = ""
 FL = ""
 W = "banaan"
 G = 0
-AL= len(W)
+AL = len(W)
 
 while True:
-    print("Welkom bij galgje, probeer het woord te raden, als je wil stoppen typ QQ, als je het woord wil raden typ ?.")
+    print("Welkom bij galgje, probeer het woord te raden, als je wil stoppen typ qq, als je het woord wil raden typ ?.")
     L = input("Geef een letter")
     L = L.lower()
+    LL = len(L)
 
-    if (L == "?"):
+    if (L == "qq"):
+        print("Doei")
+        break
+
+    elif not (LL == 1):
+        print("ERROR: Je mag niet meer dan een letter invoeren")
+
+    elif (L == "?"):
         GW = input("Wat denk je dat het woord is?")
         GW = GW.lower()
         if (GW == W) :
@@ -20,15 +28,16 @@ while True:
             G = G + 1
             print("Je goed geraden letters zijn: " + GL)
             print("Je fout geraden letters zijn: " + FL)
+
     elif not L.isalpha():
         print("ERROR: Je mag alleen letters invoeren")
-    if (L == "QQ"):
-        print("Doei")
-        break
-    if L in GL:
+
+    elif L in GL:
         print("Deze letter heb je al een keer geraden.")
+
     elif L in FL:
         print("Deze letter heb je al een keer geraden.")
+
     elif L in W:
         GL = GL + L
         AGL = len(GL)
@@ -44,12 +53,14 @@ while True:
                 print("Sorry, dat was fout.")
                 G = G + 1
                 print("Je goed geraden letters zijn: " + GL)
+
     else:
         print("Dat was fout")
         G = G + 1
         FL = FL + L
         print("Je goed geraden letters zijn: " + GL)
         print("Je fout geraden letters zijn: " + FL)
+
     if (G == 5):
         print("Sorry je hebt verloren, het woord was " + W)
         break
